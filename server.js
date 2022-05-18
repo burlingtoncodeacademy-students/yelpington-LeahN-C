79e074b965059b01ee97c0ae955c10976f2d5626
Leah N-C <Lniemcav@gmail.com>
2022-05-17 22:33:28 -0400

    Changed mongoose.connect parameter to make sure it's a string.

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const path = require("path");
 const { request, response } = require("express");
 
 //Connecting to database
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+mongoose.connect(`${process.env.MONGODB_CONNECTION_STRING}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
